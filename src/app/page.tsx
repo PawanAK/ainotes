5944a484b6a0077a84100e2c834f68b8e537b293
PawanAK <pawanajjark@gmail.com>
2024-07-02 13:01:29 +0530

    add Typewritter

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,23 @@
 import { Button } from "@/components/ui/button";
+import TypewriterTitle from "@/components/ui/TypewriterTitle";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
-  return <Button> Click Me</Button>;
+  return (
+    <div className="bg-gradient-to-r min-h-screen from-rose-100 grainy to-teal-100">
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+        <h1 className="font-semibold text-7xl text-center">
+          AI <span className="text-green-400 font-bold">Note Taking</span>{" "}
+          Assistant
+        </h1>
+        <div className="mt-4"></div>
+        <h2 className="font-semibold text-3xl text-center text-slate-700">
+          <TypewriterTitle />
+        </h2>
+        <div className="mt-8"></div>
+        <Link href="/dashboard"></Link>
+      </div>
+    </div>
+  );
 }
